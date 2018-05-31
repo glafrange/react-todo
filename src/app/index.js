8068aa5b80d13e3b9e5c94a66302df8ee5ac1242
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-30 22:37:51 -0400

    Video 3 - React Components

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-alert("it works");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Create Component
+class TodoComponent extends React.Component {
+  render(){
+    return(
+      <h1>Helooooooooo</h1>
+    );
+  }
+};
+
+// Put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
