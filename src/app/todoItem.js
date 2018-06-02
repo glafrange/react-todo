a9ceb4ad78eaba194cf0397e74bebd25843d217e
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-06-01 22:49:28 -0400

    Made todos editable

--- src/app/todoItem.js
+++ src/app/todoItem.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 require('./css/todoItem.css');
 
 class TodoItem extends React.Component {
@@ -6,6 +7,9 @@ class TodoItem extends React.Component {
     super(props);
     this.state = {};
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   };
 
   //render
@@ -13,7 +17,8 @@ class TodoItem extends React.Component {
     return(
       <li>
         <div className="todo-item">
-          <span className="item-name" onClick={this.tst}>{this.props.todo}</span>
+          <span className="item-name" ref="todoDisplay" onDoubleClick={this.handleEdit}>{this.props.todo}</span>
+          <input className="item-input" ref="editField" onBlur={this.handleSubmit} onKeyDown={this.handleKeyDown}/>
           <span className="item-delete" onClick={this.handleDelete}> X</span>
         </div>
       </li>
@@ -24,6 +29,26 @@ class TodoItem extends React.Component {
   handleDelete() {
     this.props.onDelete(this.props.todo);
   };
+
+  handleEdit(e) {
+    e.currentTarget.style.display = "none";
+    var editField = ReactDOM.findDOMNode(this.refs.editField);
+    editField.value = e.currentTarget.innerHTML;
+    editField.style.display = "block";
+  }
+
+  handleSubmit(e) {
+    e.currentTarget.style.display = "none";
+    var todoDisplay = ReactDOM.findDOMNode(this.refs.todoDisplay);
+    todoDisplay.innerHTML = e.currentTarget.value;
+    todoDisplay.style.display = "block";
+  }
+
+  handleKeyDown(e) {
+    if (e.which === 13) {
+      this.handleSubmit(e);
+    }
+  }
 };
 
 module.exports = TodoItem;
